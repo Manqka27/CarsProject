7d137d461115860169c241eb34f1cd5a12908cb1
Teodor <teolegocity@gmail.com>
2022-03-20 22:21:07 +0200

    added background

--- CarsProject/Views/CarsView.Designer.cs
+++ CarsProject/Views/CarsView.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace CarsProject
+﻿namespace CarsProject.Views
 {
-    partial class Form1
+    partial class CarsView
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CarsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CarsProject.Properties.Resources._670380;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "CarsView";
+            this.Text = "CarsView";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- CarsProject/Views/CarsView.cs
+++ CarsProject/Views/CarsView.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CarsProject
+namespace CarsProject.Views
 {
-    public partial class Form1 : Form
+    public partial class CarsView : Form
     {
-        public Form1()
+        public CarsView()
         {
             InitializeComponent();
         }
